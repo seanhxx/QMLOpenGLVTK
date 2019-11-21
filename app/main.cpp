ca398abd8e98859a30875e4b91d004ac43352109
Hu Xiaoxiang <seanhxx42@live.com>
2019-11-21 18:56:09 +0800

    add basic qt build

--- app/main.cpp
+++ app/main.cpp
@@ -1,6 +1,18 @@
-#include <iostream>
+#include <QDebug>
+#include <QApplication>
+#include <QQmlApplicationEngine>
 
-int main(int argc, char **argv){
-    std::cout << "Hello World" << std::endl;    
-    return 0;
+int main(int argc, char **argv)
+{
+    qDebug() << "Program Start!";
+    QApplication app(argc, argv);
+    QQmlApplicationEngine engine;
+    engine.load(QUrl(QStringLiteral("qrc:/app.qml")));
+    
+    if(engine.rootObjects().isEmpty())
+    {
+        return -1;
+    }
+
+    return app.exec();
 }
---
+++ include/QmlVTKRenderWindowInteractor.h
@@ -0,0 +1,5 @@
+#ifndef QmlVTKRenderWindowInteractor_h
+#define QmlVTKRenderWindowInteractor_h
+
+
+#endif
